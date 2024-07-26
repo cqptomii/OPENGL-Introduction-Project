83511320d8c46b12cf3312dccdc388252cdd755e
Tom <tomfraisse.pro@gmail.com>
2024-07-27 01:58:34 +0200

    edit Scene Class

--- Scene.cpp
+++ Scene.cpp
@@ -1,11 +1,16 @@
 #include "Scene.h"
-
+double LastX, LastY;
+double deltaTime = 0, lastFrame = 0;
 Scene::Scene(int width, int heigth,std::string name,bool UseDepth): ScreenWidth(width), ScreenHeigth(heigth), UseDepth(UseDepth) {
 	CreateScreen(name);
 }
 void Scene::DrawScene() {
 	while (!glfwWindowShouldClose(this->window))
 	{
+		float currentTime = (float)glfwGetTime();
+		deltaTime = currentTime - lastFrame;
+		lastFrame = currentTime;
+
 		process_input(this->window);
 
 		glClearColor(0.2f, 0.4f, 0.3f, 1.0f);
@@ -55,20 +60,52 @@ void Scene::CreateScreen(std::string windowName) {
 		glEnable(GL_DEPTH_TEST);
 	}
 }
-float Scene::getMouseSensivity() {
+GLFWwindow* Scene::getWindow() {
 
 }
 void window_size_callback(GLFWwindow* window, int width, int heigth) {
 	glViewport(0, 0, width, heigth);
 }
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (MainCamera.firstMove) {
+		LastX = xpos;
+		LastY = ypos;
+		MainCamera.firstMove = false;
+	}
+	double xoffset = xpos - LastX;
+	double yoffset = LastY - ypos;
+
+	LastX = xpos;
+	LastY = ypos;
+
+	float sensivity = MainCamera.getMouseSensivity();
 	
+	xoffset *= sensivity;
+	yoffset *= sensivity;
+
+	MainCamera.setYaw(MainCamera.getYaw() + xoffset);
+	MainCamera.setPitch(MainCamera.getPitch() + yoffset);
+
+	MainCamera.reloadCamParameter();
 }
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-
+	MainCamera.setFOV(MainCamera.getFOV() + yoffset);
 }
 void process_input(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	 }
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		MainCamera.handleCamInput(LEFT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		MainCamera.handleCamInput(RIGTH, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		MainCamera.handleCamInput(FORWARD, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		MainCamera.handleCamInput(BACKWARD, deltaTime);
+	}
+
 }
--- Scene.h
+++ Scene.h
@@ -5,28 +5,33 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "Camera.h"
 
 enum OBJ_TYPE {
 	Cube
 };
+
+Camera MainCamera = Camera();
+
 class Scene
 {
 private:
 	int ScreenWidth;
 	int ScreenHeigth;
 	GLFWwindow* window;
-
 public:
 	bool UseDepth = true;
 
 	void CreateScreen(std::string windowName);
-	Scene(int width,int heigth,std::string name,bool UseDepth);
+	Scene(int width, int heigth, std::string name, bool UseDepth);
 	void DrawScene();
 	void addObject(OBJ_TYPE type);
 	void setFOV(float fov);
 	float getFOV();
 	void setMouseSensivity(float sens);
 	float getMouseSensivity();
+	GLFWwindow* getWindow();
+
 };
 void window_size_callback(GLFWwindow* window, int width, int heigth);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
